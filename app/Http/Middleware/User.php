aaf5c86ed75abbc6df03b1d48df1a9493e2b719a
Chukwunonso Frank <franknonso114@gmail.com>
2026-04-20 18:22:53 +0100

    wip

--- app/Http/Middleware/User.php
+++ app/Http/Middleware/User.php
@@ -1,28 +1,22 @@
 <?php
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Redis;
 use Symfony\Component\HttpFoundation\Response;
 
 class User
 {
-    public function handle(Request $request, Closure $next): Response
-    {
-        if (Auth::user() && !Auth::user()->is_admin) {
-            $user = Auth::user();
+  public function handle(Request $request, Closure $next): Response
+  {
+    if (Auth::user() && !Auth::user()->is_admin) {
+      $user = Auth::user();
 
-            // Track this session
-            Redis::connection()->sadd(
-                "user:{$user->id}:sessions",
-                session()->getId(),
-            );
-
-            return $next($request);
-        }
-
-        abort(403, "Unauthorized");
+      return $next($request);
     }
+
+    abort(403, "Unauthorized");
+  }
 }

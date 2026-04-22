ffe3b63a220f60ed63ea67284c17be6be644e23b
Chukwunonso Frank <franknonso114@gmail.com>
2026-04-22 17:46:26 +0100

    wip

--- routes/console.php
+++ routes/console.php
@@ -3,7 +3,7 @@
 use App\Jobs\ResetDailyTasks;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new ResetDailyTasks)->everyMinute()->withoutOverlapping();
+Schedule::job(new ResetDailyTasks)->daily()->at('00:00')->withoutOverlapping();
 
 // Schedule::command('app:reset-daily-tasks')
 //     ->daily()

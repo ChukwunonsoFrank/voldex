bce118ace8b375bf44d3b3599efa557ca19e6b67
Kanu Chukwunonso <franknonso114@gmail.com>
2026-04-27 05:54:34 +0100

    change schedule duration to 10 mins

--- routes/console.php
+++ routes/console.php
@@ -3,4 +3,4 @@
 use App\Jobs\ResetDailyTasks;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new ResetDailyTasks)->everyFifteenMinutes()->withoutOverlapping();
+Schedule::job(new ResetDailyTasks)->everyTenMinutes()->withoutOverlapping();

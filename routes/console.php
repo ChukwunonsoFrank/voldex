a534a70365cb7cdbae0f83d4b8680003537ff3ec
Kanu Chukwunonso <franknonso114@gmail.com>
2026-04-27 05:07:01 +0100

    change scheduler interval

--- routes/console.php
+++ routes/console.php
@@ -3,10 +3,4 @@
 use App\Jobs\ResetDailyTasks;
 use Illuminate\Support\Facades\Schedule;
 
-Schedule::job(new ResetDailyTasks)->daily()->at('00:00')->withoutOverlapping();
-
-// Schedule::command('app:reset-daily-tasks')
-//     ->daily()
-//     ->at('00:00')
-//     ->withoutOverlapping()
-//     ->emailOutputTo('steeleluke815@gmail.com');
+Schedule::job(new ResetDailyTasks)->everyFifteenMinutes()->withoutOverlapping();
